a8f7ff17c01b66d7fee7df9dabbd3404239a4cf3
Artem Bolshak <artem.bolshak@eosda.com>
2023-02-05 21:55:35 +0200

    Solution

--- src/app.js
+++ src/app.js
@@ -1,1 +1,25 @@
 'use strict';
+
+function manipulateParams() {
+  const mappedParams = process.argv.slice(3).map(el => +el);
+  const filtered = mappedParams.filter(el => !isNaN(el));
+
+  // eslint-disable-next-line no-console
+  console.log(filtered);
+
+  switch (process.argv[2]) {
+    case 'add':
+      return filtered.length
+        ? filtered.reduce((prev, next) => prev + next, 0)
+        : 'There is nothing to add';
+    case 'multiply':
+      return filtered.length
+        ? filtered.reduce((prev, next) => prev * next, 1)
+        : 'There is nothing to multiply';
+    default:
+      return 'Operation is not supported!';
+  }
+}
+
+// eslint-disable-next-line no-console
+console.log(manipulateParams());
